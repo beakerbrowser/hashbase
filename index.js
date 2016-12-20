b63e6d2363abc6bc1d1f8a726f68cd419e8e6d19
Paul Frazee <pfrazee@gmail.com>
2016-12-20 14:41:08 -0600

    restructure to 1) support memdb option 2) export an api, with bin.js being the CLI entrypoint

--- index.js
+++ index.js
@@ -1,58 +1,59 @@
-var http = require('http')
 var level = require('level-party')
+var memdb = require('memdb')
 var township = require('township')
 var express = require('express')
 var bodyParser = require('body-parser')
-
-var config = require('./config')
 var hypercloud = require('./lib/cloud')
 
 const DAT_HASH_REGEX = /[0-9a-f]{64}$/
 
-var db = level(config.township.db)
-var ship = township(config.township, db)
-var cloud = hypercloud(config.cloud)
-var app = express()
+module.exports = function (config) {
+  var db = config.township.memdb ? memdb() : level(config.township.db)
+  var ship = township(config.township, db)
+  var cloud = hypercloud(config.cloud)
 
-app.use(bodyParser.json())
+  var app = express()
+  app.cloud = cloud
+  app.config = config
 
-app.get('/', (req, res) => {
-  res.send('HYPERCLOUD - p2p + cloud')
-})
+  app.use(bodyParser.json())
 
-app.post('/v1/register', (req, res) => {
-  ship.register(req, res, { body: req.body }, (err, code, obj) => {
-    if (err) return res.status(code).send(err.message)
-    res.status(code).json(obj)
+  app.get('/', (req, res) => {
+    res.send('HYPERCLOUD - p2p + cloud')
   })
-})
 
-app.post('/v1/profile', (req, res) => {
-  ship.verify(req, res, (err, decoded, token) => {
-    if (err) return res.status(400).send(err.message)
-    if (!decoded) return res.status(403).send('Not authorized')
-    cloud.addUser(req, res, (err, code, data) => {
+  app.post('/v1/register', (req, res) => {
+    ship.register(req, res, { body: req.body }, (err, code, obj) => {
       if (err) return res.status(code).send(err.message)
-      res.status(code).json(data)
+      res.status(code).json(obj)
     })
   })
-})
 
-app.post('/v1/login', (req, res) => {
-  ship.login(req, res, { body: req.body }, (err, code, token) => {
-    if (err) return res.status(code).send(err.message)
-    res.status(code).send(token)
+  app.post('/v1/profile', (req, res) => {
+    ship.verify(req, res, (err, decoded, token) => {
+      if (err) return res.status(400).send(err.message)
+      if (!decoded) return res.status(403).send('Not authorized')
+      cloud.addUser(req, res, (err, code, data) => {
+        if (err) return res.status(code).send(err.message)
+        res.status(code).json(data)
+      })
+    })
   })
-})
 
-app.post('/v1/logout', (req, res) => {
-  // TODO
-})
+  app.post('/v1/login', (req, res) => {
+    ship.login(req, res, { body: req.body }, (err, code, token) => {
+      if (err) return res.status(code).send(err.message)
+      res.status(code).send(token)
+    })
+  })
 
-app.get(DAT_HASH_REGEX, (req, res) => {
-  cloud.dat.httpRequest(req, res)
-})
+  app.post('/v1/logout', (req, res) => {
+    // TODO
+  })
 
-http.createServer(app).listen(config.port, () => {
-  console.log(`server started on http://127.0.0.1:${config.port}`)
-})
+  app.get(DAT_HASH_REGEX, (req, res) => {
+    cloud.dat.httpRequest(req, res)
+  })
+
+  return app
+}
