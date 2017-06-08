a7becf751d491624edb1acfd4c826840825adbad
Tara Vancil <tbvanc@gmail.com>
2017-06-07 22:16:33 -0500

    Simplify admin tool for removing an archive

--- assets/js/archive-admin.js
+++ assets/js/archive-admin.js
@@ -2,43 +2,17 @@
 
 // admin tools on archive
 $(function () {
-  var adminRemoveForm = $('#admin-remove-archive-form')
-
-
-  $('#show-admin-remove-archive-form').on('click', function () {
-    adminRemoveForm.addClass('open')
-  })
-
-  $('#cancel-admin-remove-archive').on('click', function (e) {
-    e.preventDefault()
-    adminRemoveForm.removeClass('open')
-  })
-
-  adminRemoveForm.on('submit', function (e) {
-    e.preventDefault()
-
-    // serialize form values
-    var values = {}
-    $(this).serializeArray().forEach(function (value) {
-      values[value.name] = value.value
-    })
-
-    // post to api
-    var xhr = $.post('/v1/admin/archives/' + values['key'] + '/remove', values)
-
-    xhr.done(function (res) {
-      // success, redirect
-      window.location = '/profile'
-    })
-
-    xhr.fail(function (res) {
-      // failure, render errors
-      try {
-        renderErrors(JSON.parse(res.responseText))
-      } catch (e) {
-        renderErrors(res.responseText)
-      }
-    })
+  $('#admin-remove-archive').on('click', function () {
+    if (confirm('Remove this archive?')) {
+      $.post('/v1/admin/archives/' + params.key + '/remove', {key: params.key}, function (response, status) {
+        if (status !== 'success') {
+          console.error(status, response)
+        }
+        window.location = '/' + params.owner
+      })
+    } else {
+      return
+    }
   })
 
   $('#admin-toggle-featured').click(function () {
@@ -50,9 +24,4 @@ $(function () {
       location.reload()
     })
   })
-
-  function renderErrors (json) {
-    // general error
-    $('#error-admin-general').text(json.message || json)
-  }
 })
