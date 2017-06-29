a8e784e8d91ea0e650a609406ca153e7ea7945fd
Tara Vancil <tbvanc@gmail.com>
2017-06-28 21:36:05 -0500

    Style modal form element

---
+++ assets/js/report.js
@@ -0,0 +1,45 @@
+// report form js
+$(function () {
+  $('#show-report-archive-form').on('click', showReportArchiveForm)
+  $('#report-archive-form').on('submit', submitReport)
+
+  $('#cancel-report-btn').on('click', hideForm)
+
+  function hideForm () {
+    $('.modal-form-container').removeClass('visible')
+  }
+
+  function showReportArchiveForm () {
+    $('.modal-form-container').addClass('visible')
+  }
+
+  function submitReport (e) {
+    e.preventDefault()
+
+    // serialize form values
+    var values = {}
+    $(this).serializeArray().forEach(function (value) {
+      values[value.name] = value.value
+    })
+
+    var xhr = $.post('/v1/reports/add', values)
+    xhr.done(function (res) {
+      hideForm()
+      $('#feedback-general').text('Thanks, your report has been sent to the Hashbase admins')
+    })
+
+    xhr.fail(function (res) {
+      // failure, render errors
+      try {
+        renderErrors(JSON.parse(res.responseText))
+      } catch (e) {
+        renderErrors(res.responseText)
+      }
+    })
+  }
+
+  function renderErrors (json) {
+    // general error
+    $('form #error-general').text(json.message || json)
+  }
+})
