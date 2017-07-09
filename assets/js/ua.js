d5ff8a68d11a92ea4df0cdd9e10ca33aea53d270
Paul Frazee <pfrazee@gmail.com>
2017-07-09 10:07:43 -0500

    Make standard happy

--- assets/js/ua.js
+++ assets/js/ua.js
@@ -1,11 +1,11 @@
-/* global $ */
+/* global $ navigator localStorage */
 
 // ua js
 $(function () {
   var beakerPrompts = $('.beaker-prompt')
   var usingBeaker = navigator && navigator.userAgent.includes('BeakerBrowser')
 
-  if (!usingBeaker && localStorage.hasDismissedBeakerPrompt != 1) {
+  if (!usingBeaker && localStorage.hasDismissedBeakerPrompt !== '1') {
     beakerPrompts.forEach(function (el) {
       el.classList.remove('hidden')
       $(el).click(function (e) {
