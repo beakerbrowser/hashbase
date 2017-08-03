215104c97fa2ddfded93eabe0fb931ee7b0957ae
Paul Frazee <pfrazee@gmail.com>
2017-08-03 15:29:30 -0500

    Linter

--- assets/js/frontpage.js
+++ assets/js/frontpage.js
@@ -2,9 +2,9 @@
 
 // tabbed archives list js
 $(function () {
-  [].forEach.call(document.querySelectorAll('img[data-src]'), function(img) {
+  [].forEach.call(document.querySelectorAll('img[data-src]'), function (img) {
     img.setAttribute('src', img.getAttribute('data-src'))
-    img.onload = function() {
+    img.onload = function () {
       img.removeAttribute('data-src')
     }
   })
