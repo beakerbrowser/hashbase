3822f2343c58cbdecf2194b94736aaf2317829dc
Paul Frazee <pfrazee@gmail.com>
2017-10-31 14:57:39 -0500

    Allow PDFs on user subdomains

--- assets/js/jquery.dataTables.file-size-sorting-plugin.js
+++ assets/js/jquery.dataTables.file-size-sorting-plugin.js
@@ -1,3 +1,5 @@
+/* globals jQuery */
+
 /**
  * When dealing with computer file sizes, it is common to append a post fix
  * such as B, KB, MB or GB to a string in order to easily denote the order of
@@ -18,27 +20,26 @@
  *    } );
  */
 
-jQuery.fn.dataTable.ext.type.order['file-size-pre'] = function ( data ) {
-    var matches = data.match( /^(\d+(?:\.\d+)?)\s*([a-z]+)/i );
-    var multipliers = {
-        b:  1,
-        bytes: 1,
-        kb: 1000,
-        kib: 1024,
-        mb: 1000000,
-        mib: 1048576,
-        gb: 1000000000,
-        gib: 1073741824,
-        tb: 1000000000000,
-        tib: 1099511627776,
-        pb: 1000000000000000,
-        pib: 1125899906842624
-    };
+jQuery.fn.dataTable.ext.type.order['file-size-pre'] = function (data) {
+  var matches = data.match(/^(\d+(?:\.\d+)?)\s*([a-z]+)/i)
+  var multipliers = {
+    b: 1,
+    bytes: 1,
+    kb: 1000,
+    kib: 1024,
+    mb: 1000000,
+    mib: 1048576,
+    gb: 1000000000,
+    gib: 1073741824,
+    tb: 1000000000000,
+    tib: 1099511627776,
+    pb: 1000000000000000,
+    pib: 1125899906842624
+  }
 
-    if (matches) {
-        var multiplier = multipliers[matches[2].toLowerCase()];
-        return parseFloat( matches[1] ) * multiplier;
-    } else {
-        return -1;
-    };
-};
+  if (matches) {
+    var multiplier = multipliers[matches[2].toLowerCase()]
+    return parseFloat(matches[1]) * multiplier
+  }
+  return -1
+}
