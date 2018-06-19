7359195142e06493e5f06b814a1a2166d0b91466
Paul Frazee <pfrazee@gmail.com>
2018-06-19 17:24:29 -0500

    Add rename control to archive view page

--- assets/js/archive.js
+++ assets/js/archive.js
@@ -2,6 +2,8 @@
 
 // archive page js
 $(function () {
+  var renameForm = $('#rename-form')
+  var renameFormInput = $('#rename-form #input-name')
   var removeForm = $('#remove-archive-form')
   var urlBtns = $('.link-btns .label')
 
@@ -16,6 +18,45 @@ $(function () {
   $('#cancel-remove-archive').on('click', function (e) {
     e.preventDefault()
     removeForm.removeClass('open')
+  })
+
+  renameFormInput.on('keyup', function (e) {
+    var name = renameFormInput.val()
+    var isChanged = name !== params.name
+    var url = 'dat://' + (name ? (name + '.' + params.hostname) : params.key)
+    $('#feedback-name .is-will-be').text(isChanged ? 'will be': 'is')
+    $('#feedback-name .link').attr('href', url)
+    $('#feedback-name .link').text(name ? url : (url.slice(0, 12) + '..' + url.slice(-2)))
+    if (isChanged) $('#rename-form .btn').removeAttr('disabled')
+    else $('#rename-form .btn').attr('disabled', true)
+  })
+
+  renameForm.on('submit', function (e) {
+    e.preventDefault()
+
+    // serialize form values
+    var values = {}
+    $(this).serializeArray().forEach(function (value) {
+      values[value.name] = value.value
+    })
+    if (!values.name) {
+      delete values.name
+    }
+
+    var xhr = $.post('/v2/archives/item/' + params.key, values)
+    xhr.done(function (res) {
+      // success, redirect
+      window.location = '/' + params.owner + '/' + (values.name || params.key)
+    })
+
+    xhr.fail(function (res) {
+      // failure, render errors
+      try {
+        renderErrors(JSON.parse(res.responseText))
+      } catch (e) {
+        renderErrors(res.responseText)
+      }
+    })
   })
 
   removeForm.on('submit', function (e) {
