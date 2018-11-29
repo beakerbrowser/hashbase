ff9eaca71add8df2627cf2e74a9eb27831b4eebb
Paul Frazee <pfrazee@gmail.com>
2018-11-29 12:05:35 -0600

    Add missing indexes

--- lib/dbs/migrations/001-initial-schema.sql
+++ lib/dbs/migrations/001-initial-schema.sql
@@ -7,14 +7,10 @@ CREATE TABLE activity (
   action TEXT,
   params TEXT -- stores serialized JSON
 );
+CREATE INDEX activity_username_idx ON activity(username);
 CREATE TABlE archives (
   key TEXT PRIMARY KEY,
   isFeatured INTEGER, -- flag
-
-  -- denormalized data
-  -- TODO do we need this now that we have sqlite?
-  name TEXT,
-  ownerName TEXT,
 
   -- stats
   diskUsage INTEGER DEFAULT 0,
@@ -26,6 +22,7 @@ CREATE TABlE archives (
   updatedAt INTEGER DEFAULT (strftime('%s', 'now')),
   createdAt INTEGER DEFAULT (strftime('%s', 'now'))
 );
+CREATE INDEX archives_createdAt_idx ON archives(createdAt);
 CREATE TABLE reports (
   id INTEGER PRIMARY KEY,
   archiveKey TEXT NOT NULL,
@@ -40,6 +37,9 @@ CREATE TABLE reports (
 
   FOREIGN KEY (archiveKey) REFERENCES archives (key)
 );
+CREATE INDEX reports_archiveOwner_idx ON reports(archiveOwner);
+CREATE INDEX reports_reportingUser_idx ON reports(reportingUser);
+CREATE INDEX reports_archiveKey_idx ON reports(archiveKey);
 CREATE TABLE users (
   id TEXT PRIMARY KEY,
   username TEXT NOT NULL,
@@ -87,10 +87,17 @@ CREATE TABLE users_archives (
   FOREIGN KEY (userid) REFERENCES users (id) ON DELETE CASCADE,
   FOREIGN KEY (key) REFERENCES archives (key) ON DELETE CASCADE
 );
+CREATE UNIQUE INDEX users_archives_name_idx ON users_archives(name);
 
 -- Down
+DROP INDEX users_archives_name_idx;
 DROP TABLE users_archives;
 DROP TABLE users;
+DROP INDEX reports_archiveOwner_idx;
+DROP INDEX reports_reportingUser_idx;
+DROP INDEX reports_archiveKey_idx;
 DROP TABLE reports;
+DROP INDEX archives_createdAt_idx;
 DROP TABlE archives;
+DROP INDEX activity_username_idx;
 DROP TABLE activity;
